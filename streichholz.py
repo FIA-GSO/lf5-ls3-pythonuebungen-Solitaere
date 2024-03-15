b4540ba1f235de2bb5bcdeabc282b97308ccd1fe
Solitaere <86192338+Solitaere@users.noreply.github.com>
2024-03-15 21:28:56 +0000

    added temp files

---
+++ streichholz.py
@@ -0,0 +1,19 @@
+mengeStreichholz: int  = 29;
+print("Der Computer nimmt 2 Streichhölzer. Es sind noch",mengeStreichholz,"übrig")
+while True:
+    
+    spielerStreichholz = int(input("Wie viele Streichhölzer möchtest du nehmen?"))
+    if spielerStreichholz > 6 or spielerStreichholz > mengeStreichholz:
+        print("Gib eine kleinere Zahl an (1-6)")
+        continue
+    mengeStreichholz = mengeStreichholz - spielerStreichholz
+
+    if mengeStreichholz <= 1:
+        print("Du hast verloren.")
+        break
+    compStreichholz = 7 
+    compStreichholz = compStreichholz  - spielerStreichholz
+    mengeStreichholz = mengeStreichholz - compStreichholz
+    print("Der Computer nimmt",compStreichholz,"noch",mengeStreichholz,"Streichhölzer sind vorhanden.")
+
+
